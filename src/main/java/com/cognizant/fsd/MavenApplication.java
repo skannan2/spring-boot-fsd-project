3fc7db45923d3e26f6a12b197e7b7df8bb616e35
sanjay <san_jayk@yahoo.com>
2018-08-18 12:29:31 +0530

    Initial Commit

---
+++ src/main/java/com/cognizant/fsd/MavenApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.fsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+}
